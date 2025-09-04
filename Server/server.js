7e30eb84b25d9155297512e9cfa69a5410f2c44e
Chaitanya <chaituchaitanya081@gmail.com>
2025-09-04 16:51:04 +0530

    first commit

--- Server/server.js
+++ Server/server.js
@@ -31,7 +31,8 @@ app.use('/api',userRouter)
 
 
 // connect to a port
-app.listen('5000',()=>{
+const PORT=process.env.PORT || 5000
+app.listen(PORT,()=>{
     console.log('Server is listening on port 5000')
 })
 
